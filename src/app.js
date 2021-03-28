2eb28319fc12915b70715617739e8ebe50e111bc
Claudio Vallejos <claudio.v18@hotmail.cl>
2021-03-27 23:47:40 -0300

    frontenddeploy

---
+++ src/app.js
@@ -0,0 +1,79 @@
+
+const search = document.getElementById("search");
+const url = "https://nodejs-bsale-api.herokuapp.com";
+/* */
+search.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    divProducts.innerHTML = "";
+    fetchSomeProduct(e.target.value);
+  }
+});
+
+const apiSearch = (url) => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      res.forEach((prod) => {
+        printProduct(prod);
+      });
+    });
+};
+
+/* FETCH DATA*/
+export const fetchProductsData = () => {
+  let changeableUrl = url;
+  try {
+    apiSearch(changeableUrl);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchSomeProduct = (nameToSearch) => {
+  let changeableUrl = url;
+
+  if (nameToSearch) {
+    const name = encodeURIComponent(nameToSearch);
+    changeableUrl = `${url}/product/search/${name}`;
+  }
+  try {
+    apiSearch(changeableUrl);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/* SCREENS */
+const printProduct = (prod) => {
+  const product = `
+      <li class="product-content">
+          <div class="product">
+              <a href="#">
+                  <img
+                      ngIf="prod"
+                      src="${
+                        prod.url_image
+                          ? prod.url_image
+                          : "./assets/imagen-no-disponible.png"
+                      }"
+                      alt="product${prod.id}" />
+              </a>
+              <hr>
+            <div class="product-name">
+                <a id="productName" value="${prod.id}" href="">
+                  ${prod.name}
+                </a>
+            </div>
+              <div class="product-price">
+                <div> $ ${prod.price} </div>
+                   <button class="btn-cart"><i class="bi bi-cart-plus"></i></button>
+              </div>
+          </div>
+
+      </li>
+
+  `;
+  divProducts.insertAdjacentHTML("beforeend", product);
+};
+
+fetchProductsData();
---
+++ src/readme.md
@@ -0,0 +1,9 @@
+-crear layout estático HTML / OK!
+-hacer el css ddel layout   / OK!
+-hacer fetch de la api en consola.
+-cargar todos los productos     / OK!
+    -fetch a api con la ruta    /   OK!
+    -hacer una función que pinte / innerHML /OK!
+-buscador de productos
+
+* REFACTORIZAR app.js
